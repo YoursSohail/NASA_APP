72d0486bae5811d1baf5eccc5e618c6b74e933ff
YoursSohail <yourssohail@gmail.com>
2019-12-05 01:22:50 +0530

    apod inital layout done

--- app/src/main/java/com/yourssohail/nasaapp/data/api/NasaApiClient.kt
+++ app/src/main/java/com/yourssohail/nasaapp/data/api/NasaApiClient.kt
@@ -1,4 +1,51 @@
 package com.yourssohail.nasaapp.data.api
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+
+const val API_KEY = "BW9QVZwzvHXB401BFnrbldLYSMfQaSxOxngvR7kN"
+const val BASE_URL = "https://api.nasa.gov/"
+
 class NasaApiClient {
+
+    fun getClient():NasaApiInterface{
+
+       val requestInterceptor = Interceptor{chain ->
+
+           val url = chain.request()
+               .url()
+               .newBuilder()
+               .addQueryParameter("api_key", API_KEY)
+               .build()
+
+           val request = chain.request()
+               .newBuilder()
+               .url(url)
+               .build()
+
+           return@Interceptor chain.proceed(request)
+
+       }
+
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(requestInterceptor)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build()
+
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NasaApiInterface::class.java)
+
+
+    }
+
 }
