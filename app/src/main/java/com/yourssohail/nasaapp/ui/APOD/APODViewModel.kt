5bad1f0d36adc625f1533b6c109e0d9e99938989
YoursSohail <yourssohail@gmail.com>
2019-12-05 11:18:37 +0530

    added apod view model

---
+++ app/src/main/java/com/yourssohail/nasaapp/ui/APOD/APODViewModel.kt
@@ -0,0 +1,27 @@
+package com.yourssohail.nasaapp.ui.APOD
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.yourssohail.nasaapp.data.model.APODData
+import com.yourssohail.nasaapp.data.repository.NetworkState
+import io.reactivex.disposables.CompositeDisposable
+
+class APODViewModel(
+    private val apodRepository: APODRepository
+) : ViewModel() {
+
+    private val compositeDisposable = CompositeDisposable()
+
+    val apodData : LiveData<APODData> by lazy {
+        apodRepository.getAPOD(compositeDisposable)
+    }
+
+    val apodNetworkState : LiveData<NetworkState> by lazy {
+        apodRepository.getAPODNetworkState()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
