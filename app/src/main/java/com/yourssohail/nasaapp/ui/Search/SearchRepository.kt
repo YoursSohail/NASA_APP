49b90cb1dc90ca3d6c20ab7d628548f257b9ee95
YoursSohail <yourssohail@gmail.com>
2019-12-07 12:40:12 +0530

    added SearchRepository in ui/search

---
+++ app/src/main/java/com/yourssohail/nasaapp/ui/Search/SearchRepository.kt
@@ -0,0 +1,27 @@
+package com.yourssohail.nasaapp.ui.Search
+
+import androidx.lifecycle.LiveData
+import com.yourssohail.nasaapp.data.api.search_api.SearchApiInterface
+import com.yourssohail.nasaapp.data.model.SearchData
+import com.yourssohail.nasaapp.data.repository.NetworkState
+import com.yourssohail.nasaapp.data.repository.SearchDataSource
+import io.reactivex.disposables.CompositeDisposable
+
+class SearchRepository(
+    private val apiService : SearchApiInterface
+) {
+    lateinit var searchDataSource : SearchDataSource
+
+    fun getSearchResults(compositeDisposable: CompositeDisposable,q:String):LiveData<SearchData>{
+
+        searchDataSource = SearchDataSource(apiService,compositeDisposable)
+        searchDataSource.fetchSearchResults(q)
+
+        return searchDataSource.searchData
+
+    }
+
+    fun getNetworkState():LiveData<NetworkState>{
+        return searchDataSource.networkState
+    }
+}
