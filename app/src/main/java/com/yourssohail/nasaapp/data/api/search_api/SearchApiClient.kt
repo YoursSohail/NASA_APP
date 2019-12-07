dd699b06c732358700210706439eda42692cb7b3
YoursSohail <yourssohail@gmail.com>
2019-12-07 12:31:36 +0530

    added search data source repository

--- app/src/main/java/com/yourssohail/nasaapp/data/api/search_api/SearchApiClient.kt
+++ app/src/main/java/com/yourssohail/nasaapp/data/api/search_api/SearchApiClient.kt
@@ -1,6 +1,21 @@
 package com.yourssohail.nasaapp.data.api.search_api
 
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 const val BASE_URL = "https://images-api.nasa.gov/"
 
 class SearchApiClient {
+
+    fun getClient() : SearchApiInterface{
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(SearchApiInterface::class.java)
+
+    }
 }
--- app/src/main/java/com/yourssohail/nasaapp/data/repository/SearchDataSource.kt
+++ app/src/main/java/com/yourssohail/nasaapp/data/repository/SearchDataSource.kt
@@ -1,4 +1,48 @@
 package com.yourssohail.nasaapp.data.repository
 
-class SearchDataSource {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.yourssohail.nasaapp.data.api.search_api.SearchApiInterface
+import com.yourssohail.nasaapp.data.model.SearchData
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.lang.Exception
+
+class SearchDataSource(
+    private val apiService : SearchApiInterface,
+    private val compositeDisposable: CompositeDisposable
+) {
+
+    private val _networkState = MutableLiveData<NetworkState>()
+    val networkState : LiveData<NetworkState>
+    get() = _networkState
+
+    private val _searchDate = MutableLiveData<SearchData>()
+    val searchData : LiveData<SearchData>
+    get() = _searchDate
+
+    fun fetchSearchResults(q:String){
+        _networkState.postValue(NetworkState.LOADING)
+
+        try{
+            compositeDisposable.add(
+                apiService.getSearchResults(q)
+                    .subscribeOn(Schedulers.io())
+                    .subscribe(
+                        {
+                            _searchDate.postValue(it)
+                            _networkState.postValue(NetworkState.LOADED)
+                        },
+                        {
+                            _networkState.postValue(NetworkState.ERROR)
+                            Log.e("SearchDataSource",it.message)
+                        }
+                    )
+            )
+        }catch (e:Exception){
+            Log.e("SearchDataSource",e.message)
+        }
+    }
+
 }
